f13d5e33c55a7bb6527d65e0aa06d6520ae3c679
Lachezar Kutsarov <lkutsarov@gmail.com>
2025-03-23 10:35:30 +0100

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,20 @@
+
+/**
+ * @param {string} input
+ * @param {string} pattern
+ * @return {boolean}
+ */
+var hasMatch = function (input, pattern) {
+    const NOT_FOUND = -1;
+    const SPECIAL_CHAR = '*';
+
+    const indexSpecialChar = pattern.indexOf(SPECIAL_CHAR);
+    const indexFirstPart = input.indexOf(pattern.substring(0, indexSpecialChar));
+    if (indexFirstPart === NOT_FOUND) {
+        return false;
+    }
+    const indexSecondPart = input.substring(indexFirstPart + indexSpecialChar)
+                                 .indexOf(pattern.substring(indexSpecialChar + 1));
+                         
+    return indexSecondPart !== NOT_FOUND;
+};
